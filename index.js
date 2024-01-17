b2fcf522663a450502376aebfa391234ea2b7b18
navya-99 <padiyarnavya@gmail.com>
2024-01-17 23:14:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+document.getElementById('viewResumeBtn').addEventListener('click', function() {
+  var resumePath = 'https://drive.google.com/file/d/1I9anT6lnTTQBz34VEz6Dd-cU1LfiPGtq/view';
+
+  var newTab = window.open(resumePath, '_blank');
+
+  // // Check if the new tab was successfully opened
+  // if (newTab) {
+  //   // Focus on the new tab
+  //   newTab.focus();
+
+  //   // Provide a download link in the new tab
+  //   newTab.document.write(`
+  //     <html>
+  //       <head>
+  //         <title>Resume</title>
+  //       </head>
+  //       <body>
+  //         <p>Click <a href="${resumePath}" download>here</a> to download the resume.</p>
+  //       </body>
+  //     </html>
+  //   `);
+  // } else {
+  //   // Display an error message if the new tab couldn't be opened
+  //   alert('Unable to open the resume. Please check your browser settings.');
+  // }
+});
